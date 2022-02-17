137a517ee2a7094ad60b46fa294cb40423a73037
Abbracx <tankoraphael@gmail.com>
2022-02-18 00:18:10 +0100

    A basic API using barebone Django

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductListView
+from .views import product_list, product_detail, manufacturer_list, manufacturer_detail
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product-list"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product-detail")
+    path("products/", product_list, name="product-list"),
+    path("product-detail/<int:pk>/", product_detail, name="product-list"),
+    path("manufacturers/", manufacturer_list, name="manufacturer_list"),
+    path("manufacturers/<int:pk>", manufacturer_detail, name="manufacturer_detail"),
 ]
 
 
